4c3e0acbe0446d6af8653a037a24f4a05aafbac6
successdevop <successraphael28@gmail.com>
2026-02-23 08:58:03 +0100

    this section I used string and the input function to collect data and display to users

---
+++ string.py
@@ -0,0 +1,9 @@
+print("Today is a good day to learn python")
+print('python is fun')
+print('We can include "quotes" in string')
+print("Hello" + " world")
+greeting = "Hello"
+name = input("please enter your name ")
+print(greeting+name)
+print()
+print(greeting + " " + name)
