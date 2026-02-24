13b034bcea22b8bd2f985e3a7f5e32128deba686
successdevop <successraphael28@gmail.com>
2026-02-24 09:13:33 +0100

    practised slicing with my student. Did negative slicing,positive slicing and reverse slicing

---
+++ sliceback.py
@@ -0,0 +1,17 @@
+letters = "abcdefghijklmnopqrstuvwxyz"
+
+print(letters[::-1])
+
+first_char = letters[16:13:-1]
+print(first_char)
+second_char = letters[4::-1]
+print(second_char)
+print(letters[-22::-1])
+third_char = letters[25:17:-1]
+print(third_char)
+print(letters[:-9:-1])
+print("========")
+print(letters[-4:])
+print(letters[-1:])
+print(letters[:1])
+print(letters[0])
