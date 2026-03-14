99343b1ac49ffaccf84b49085973b49a08f1921f
successdevop <successraphael28@gmail.com>
2026-03-14 10:39:46 +0100

    taught my student the concept of immutable and mutable objects in python with examples. And also the consequences of understanding how it works in writing python code

---
+++ quiz.py
@@ -0,0 +1,18 @@
+asteroids = [9617, 9618, 9619, 9620, 9621, 9622, 13681]
+
+for asteroid in asteroids:
+    if asteroid == 9617:
+        print("Grahamchapman")
+    elif asteroid == 9618:
+        print("Johncleese")
+    elif asteroid == 9619:
+        print("Terrygilliam")
+        break
+    elif asteroid == 9620:
+        print("Ericidle")
+    elif asteroid == 9621:
+        print("Michaelpalin")
+    else:
+        print("Terryjones")
+else:
+    print("MontyPython")
