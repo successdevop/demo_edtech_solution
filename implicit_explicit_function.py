8d361592c73256737b5d89eab0aeb46d0e1268f8
successdevop <successraphael28@gmail.com>
2026-03-18 20:26:12 +0100

    rewrote our code to use a function in optimizing the equation we earlier wrote

---
+++ implicit_explicit_function.py
@@ -0,0 +1,31 @@
+import random
+
+
+def get_integer(prompt):
+    while True:
+        temp = input(prompt)
+        if temp.isnumeric():
+            return int(temp)
+        print(f"{temp} is not a valid number")
+
+
+highest = 10
+answer = random.randint(1, highest)
+guess = 0
+print(f"Please guess between 1 and {highest}:")
+
+while answer != guess:
+    guess = get_integer(": ")
+
+    if guess == 0:
+        break
+
+    if guess == answer:
+        print("You guessed it correctly")
+        break
+    else:
+        if guess < answer:
+            print("Please guess higher")
+        else:
+            print("Please guess lower")
+
