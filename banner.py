c4dc15ad91c4b542d3941e64c1e30b1421a8d536
successdevop <successraphael28@gmail.com>
2026-03-19 18:42:48 +0100

    We built the fizzbuzz game to illustrate to my student how functions in python can help in writing efficient code and also very easy for debugging

--- banner.py
+++ banner.py
@@ -1,4 +1,4 @@
-def banner(text=" ", screen_width=80):
+def banner(text: str = " ", screen_width: int = 80) -> None:
     if len(text) > screen_width - 4:
         print("EEKS!!")
         print("THE TEXT IS TOO LONG TO FIT IN THE SPECIFIED SCREEN WIDTH")
@@ -10,10 +10,10 @@ def banner(text=" ", screen_width=80):
         print(output_string)
 
 
-banner(screen_width=70, text="*")
+banner(text="*")
 banner("the fox jumps over the lazy dog hello everyone")
-banner(screen_width=70)
-banner("Your idea is the basis for your future business.",66)
+banner()
+banner("Your idea is the basis for your future business.")
 banner("A business plan template can be useful")
 banner("Be realistic and honest.")
-banner("*",70)
+banner("*")
