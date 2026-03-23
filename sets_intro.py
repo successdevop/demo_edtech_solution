223f4108432ed613b32aedeabf3cbc5ffd4a23e3
successdevop <successraphael28@gmail.com>
2026-03-23 11:22:56 +0100

    taught my students when it's appropriate to use the discard and remove methods of set when writting a program or carring out an operation.

--- sets_intro.py
+++ sets_intro.py
@@ -1,20 +1,36 @@
 farms_animals = {"cow", "sheep", "hen", "goat", "horse"}
 
-numbers = {*{}}
-print(numbers)
+# numbers = {*{}}
+# print(numbers)
+#
+# number = {*""}
+# print(number)
+#
+# new_numbers = set()
+# print(new_numbers)
+#
+# new_numbers.add(3)
+# print(new_numbers)
+# print("+"*15)
+#
+# while len(numbers) < 4:
+#     val = input("Enter number: ")
+#     numbers.add(val)
+#
+# print(numbers)
 
-number = {*""}
-print(number)
+data = ["blue", "red", "blue", "green", "red", "blue", "white"]
+# print(data)
+# print()
+#
+# data_1 = sorted(set(data))
+# print(data_1)
+# print()
+#
+data_2 = set(dict.fromkeys(data))
+print(data_2)
 
-new_numbers = set()
-print(new_numbers)
+data_2.discard("red")
+print(data_2)
 
-new_numbers.add(3)
-print(new_numbers)
-print("+"*15)
-
-while len(numbers) < 4:
-    val = input("Enter number: ")
-    numbers.add(val)
-
-print(numbers)
+data_2.remove("pink")
