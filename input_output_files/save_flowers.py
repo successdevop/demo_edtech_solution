644072d93cc8c0aa4eec6c9ddfef73381385f2b7
successdevop <successraphael28@gmail.com>
2026-03-27 09:23:53 +0100

    taught my students about ASCII and unicode strings, the history and practical application of both

--- input_output_files/save_flowers.py
+++ input_output_files/save_flowers.py
@@ -22,13 +22,27 @@ data = [
 
 flower_list = []
 
-with open("flowers_print.txt") as flowers:
-    for flower in flowers:
-        flower_list.append(flower.rstrip())
+# with open("flowers_print.txt") as flowers:
+#     for flower in flowers:
+#         flower_list.append(flower.rstrip())
+#
+# print(flower_list)
+# print("+="*80)
 
-print(flower_list)
-print("+="*80)
-plants_filename = "flowers_print.txt"
-with open(plants_filename, "w") as plants:
-    for plant in data:
-        print(plant, file=plants)
+# plants_filename = "flowers_print.txt"
+# with open(plants_filename, "w") as plants:
+#     for plant in data:
+#         print(plant, file=plants)
+
+
+# plant_text = "flower_shrubs.txt"
+# with open(plant_text, "w") as jab_flower:
+#     for plants in data:
+#         print(plants, file=jab_flower)
+
+new_plant_list = []
+with open("flower_shrubs.txt") as jab_flower:
+    for plant in jab_flower:
+        new_plant_list.append(plant.rstrip())
+
+print(new_plant_list)
