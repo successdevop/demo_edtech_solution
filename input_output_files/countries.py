1c16eb74369148d452a5c2b7037950d0103873fd
successdevop <successraphael28@gmail.com>
2026-03-27 08:11:05 +0100

    taught my students how to text to files and also how to read text from file

--- input_output_files/countries.py
+++ input_output_files/countries.py
@@ -19,16 +19,37 @@ with open(filename) as info:
 
         countries[country.casefold()] = country_dict
 
-# print(countries)
+print(countries)
+
+missing_values_box = []
+for country_w_m_v in countries:
+    each_country = countries[country_w_m_v]
+    for key, missing_values in each_country.items():
+        if missing_values == "":
+            missing_values_box.append(key)
+    print(f"{each_country['name']} has these missing values: {', '.join(key for key in missing_values_box)}")
+    print()
+    missing_values_box.clear()
 
 
-print("=="*5 + " Capital city challenge " + "=="*5)
-user_country = input("Enter a country: ").casefold()
-if user_country in countries:
-    print(f"The capital of {user_country.capitalize()} is => {countries[user_country]['capital']}")
-else:
-    print("Country not found")
+# for country_no_capital in countries:
+#     if not countries[country_no_capital]['capital']:
+#         print(f"{country_no_capital} - {countries[country_no_capital]['capital']}")
 
+
+    # print(country_no_capital)
+    # if country_no_capital in countries:
+    #     for missing_v in countries[country_no_capital]:
+    #         if
+
+# print("==" * 5 + " Capital city challenge " + "==" * 5)
+# user_country = input("Enter a country: ").casefold()
+# if user_country in countries:
+#     print(f"{'The capital of ' + user_country.capitalize() + 'is => ' + countries[user_country]['capital'] if
+#     countries[user_country]['capital'] else user_country.capitalize() + ' does not have a capital'}")
+#     # print(f"The capital of {user_country.capitalize()} is => {countries[user_country]['capital']}")
+# else:
+#     print("Country not found")
 
 # for cty, cty_detail in countries.items():
 #     if cty.casefold() == user_country.casefold():
---
+++ input_output_files/save_flowers.py
@@ -0,0 +1,34 @@
+data = [
+    "Andromeda - Shrub",
+    "Bellflower - Flower",
+    "China Pink - Flower",
+    "Daffodil - Flower",
+    "Evening Primrose - Flower",
+    "French Marigold - Flower",
+    "Hydrangea - Shrub",
+    "Iris - Flower",
+    "Japanese Camellia - Shrub",
+    "Lavender - Shrub",
+    "Lilac - Shrub",
+    "Magnolia - Shrub",
+    "Peony - Shrub",
+    "Queen Anne's Lace - Flower",
+    "Red Hot Poker - Flower",
+    "Snapdragon - Flower",
+    "Sunflower - Flower",
+    "Tiger Lily - Flower",
+    "Witch Hazel - Shrub",
+]
+
+flower_list = []
+
+with open("flowers_print.txt") as flowers:
+    for flower in flowers:
+        flower_list.append(flower.rstrip())
+
+print(flower_list)
+print("+="*80)
+plants_filename = "flowers_print.txt"
+with open(plants_filename, "w") as plants:
+    for plant in data:
+        print(plant, file=plants)
