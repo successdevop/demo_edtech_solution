1e6c0c016dc614bed448a3eab145837c84e9d7c6
successdevop <successraphael28@gmail.com>
2026-03-29 19:12:33 +0100

    taught my student how to write to a csv file using python

--- input_output_files/country_dialect.py
+++ input_output_files/country_dialect.py
@@ -3,8 +3,27 @@ import csv
 csv_filename = "country_info.txt"
 
 with open(csv_filename, encoding='utf-8', newline='') as csv_file:
-    sample = csv_file.read()
+    sample = ""
+    for i in range(3):
+        sample += csv_file.readline()
+
     data = csv.Sniffer().sniff(sample)
-    reader = csv.reader(csv_file)
+    csv_file.seek(0)
+    reader = csv.reader(csv_file, dialect=data)
     for row in reader:
         print(row)
+
+print("*"*80)
+
+attributes = [
+    "delimiter",
+    "doublequote",
+    "escapechar",
+    "lineterminator",
+    "quotechar",
+    "quoting",
+    "skipinitialspace"
+]
+
+for attribute in attributes:
+    print(f"{attribute}: {getattr(data, attribute)}")
