1f1b726549d777be7f144b066cfa7ba34af1bc7a
successdevop <successraphael28@gmail.com>
2026-04-06 20:25:12 +0100

    OOP concept

---
+++ oop/oop_intro.py
@@ -0,0 +1,38 @@
+class Kettle(object):
+    def __init__(self, make, price):
+        self.make = make
+        self.price = price
+        self.on = False
+
+    def switch_on(self):
+        self.on = True
+
+
+kenwood = Kettle("Kenwood", 8.99)
+print(kenwood.make)
+print(kenwood.price)
+kenwood.price = 12.55
+print(kenwood.price)
+
+hamilton = Kettle("Hamilton", 14.55)
+print("Models: {} = {}, {} = {}".format(kenwood.make, kenwood.price, hamilton.make, hamilton.price))
+
+print("================")
+print("Model: {0.make}={0.price} : {1.make}={1.price}".format(kenwood, hamilton))
+print(hamilton.on)
+hamilton.switch_on()
+print(hamilton.on)
+print(kenwood.on)
+
+print("=============")
+Kettle.switch_on(kenwood)
+print(hamilton.on)
+print(kenwood.on)
+print("==============")
+kenwood.power = 1.5
+print(kenwood.power)
+print(hamilton.power)
+
+
+
+
