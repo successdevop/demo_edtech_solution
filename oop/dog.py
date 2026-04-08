ddef8c9fb112393a2172226165c1c0819b695eac
successdevop <successraphael28@gmail.com>
2026-04-08 17:01:48 +0100

    .

--- oop/dog.py
+++ oop/dog.py
@@ -1,25 +1,50 @@
 class Dog:
     species = "Canis familiaris"
 
-    def __init__(self, name, age):
+    def __init__(self, name: str, age: int):
         self.name = name
         self.age = age
 
     def __str__(self):
         return f"{self.name} is {self.age} years old"
 
-    def sound(self, sound: str):
-        print(f"{self.name} says {sound}")
+    def speak(self, sound: str):
+        return f"{self.name} barks: {sound}"
 
 
-bingo = Dog("Bingo", 4)
-bingo.sound("'wooooo'")
-print(bingo)
+class JackRussellTerrier(Dog):
+    def speak(self, sound = "Arf"):
+        return f"{self.name} says {sound}"
 
-puppy = Dog("Puppy", 6)
-#
-# print(bingo)
-# print(puppy)
-# print(bingo == puppy)
-#
-#
+
+class Dachshund(Dog):
+    pass
+
+
+class Bulldog(Dog):
+    pass
+
+
+miles = JackRussellTerrier("Miles", 4)
+buddy = Bulldog("Buddy", 9)
+jack = Bulldog("Jack", 3)
+jim = Dachshund("Jim", 5)
+
+# print(isinstance(miles, Dog))
+# print(isinstance(miles, JackRussellTerrier))
+print(miles.speak("Grrr"))
+print(jim.speak("Woof woof"))
+
+
+class Car:
+    def __init__(self, brand):
+        self.brand = brand
+
+    def show(self):
+        print(f"{self.brand}")
+
+# Create an object
+c1 = Car("Ford")
+
+# Call the show method
+c1.show()
